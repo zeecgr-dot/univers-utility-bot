61f9b9e5e5551a8ae2facbbfca63e22edd074cbe
zeecgr-dot <zeec.gr@gmail.com>
2026-05-23 16:42:33 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const canal = '@universpromo';
 
 console.log('BOT ONLINE 🚀');
 
-bot.on('message', async (msg) => {
+bot.on('message', (msg) => {
 
   if (!msg.text) return;
 
@@ -17,19 +17,18 @@ bot.on('message', async (msg) => {
   const linhas = msg.text.split('\n');
 
   const produto = linhas[0] || 'Produto';
-  const preco = linhas[1] || 'Preço não informado';
+  const preco = linhas[1] || 'Preço';
   const link = linhas[2] || '';
 
-  const mensagem = `
-🔥 OFERTA DO DIA
+  const mensagem =
+`🔥 OFERTA DO DIA
 
 🛍️ ${produto}
 💸 ${preco}
 
 🔗 ${link}
 
-⚡ corre antes que acabe
-`;
+⚡ corre antes que acabe`;
 
   bot.sendMessage(canal, mensagem);
 

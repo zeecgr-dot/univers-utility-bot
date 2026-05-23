78564a2a7f65cb156ce2152bc8ebee192550b66a
zeecgr-dot <zeec.gr@gmail.com>
2026-05-23 16:39:21 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = 
-'8605464110:AAHggkvlL23mDHcXnRjY_pQKxUoskTvCThc';
+const token = 'SEU_TOKEN_AQUI';
 
 const bot = new TelegramBot(token, { polling: true });
 
 const canal = '@universpromo';
 
-bot.sendMessage(canal, '🚀 BOT ONLINE');
+console.log('BOT ONLINE 🚀');
 
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
 
   if (!msg.text) return;
 
   if (msg.text.startsWith('/')) return;
 
-  bot.sendMessage(canal, msg.text);
+  const linhas = msg.text.split('\n');
+
+  const produto = linhas[0] || 'Produto';
+  const preco = linhas[1] || 'Preço não informado';
+  const link = linhas[2] || '';
+
+  const mensagem = `
+🔥 OFERTA DO DIA
+
+🛍️ ${produto}
+💸 ${preco}
+
+🔗 ${link}
+
+⚡ corre antes que acabe
+`;
+
+  bot.sendMessage(canal, mensagem);
 
 });

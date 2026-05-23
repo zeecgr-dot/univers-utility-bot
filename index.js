7024415c2e676010089155b8b355cb30b5aed164
zeecgr-dot <zeec.gr@gmail.com>
2026-05-23 15:02:45 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const TelegramBot = require("node-telegram-bot-api");
 
-const token = "SEU_TOKEN_AQUI";
-
-const bot = new TelegramBot(token);
-
-bot.sendMessage("@SEU_CANAL", "🚀 BOT ONLINE");
+8605464110:AAHggkvlL23mDHcXnRjY_pQKx
+UoskTvCThc
